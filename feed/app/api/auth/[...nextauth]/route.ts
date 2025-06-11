1f24f336e7c7dfd979c45cb285e19a6333b2fc11
colindharrington <colindavidharrington@gmail.com>
2025-06-11 13:19:49 -0400

    nice

--- feed/app/api/auth/[...nextauth]/route.ts
+++ feed/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,13 @@ export const authConfig = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "select_account",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
   pages: {

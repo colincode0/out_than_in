bb84213ddd9cbf943202f0d87fcaa092e5ee8569
colindharrington <colindavidharrington@gmail.com>
2025-06-13 16:46:45 -0400

    individual post page

--- feed/app/layout.tsx
+++ feed/app/layout.tsx
@@ -5,6 +5,7 @@ import { Providers } from "./providers";
 import { getServerSession } from "next-auth";
 import { authConfig } from "./auth";
 import Drawer from "./components/Drawer";
+import PostButton from "./components/PostButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,6 +41,7 @@ export default async function RootLayout({
       >
         <Providers session={session}>
           <Drawer />
+          <PostButton />
           {children}
         </Providers>
       </body>
